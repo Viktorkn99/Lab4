47841027707e476223d30094d4e0d605f4b80957
Viktorkn99 <knutson@live.se>
2025-03-14 10:47:39 +0100

    Updaterade Controller med alla tester

--- Controller.java
+++ Controller.java
@@ -16,8 +16,56 @@ public class Controller {
 
 
     public void onTilePressed(int row, int col) {
-        System.out.println("Test grid");
+        System.out.println("Test grid row = " + row + " col = " + col);
     }
+
+    public void onNewGamePressed() {
+        System.out.println("Test new game");
+    }
+
+    public void onDifficultyPressed() {
+        System.out.println("Tests difficulty");
+    }
+
+    public void onLoadGamePressed() {
+        System.out.println("Test Load game");
+    }
+
+    public void onSaveGamePressed() {
+        System.out.println("Test Save game");
+    }
+    public void onExitPressed() {
+        System.out.println("Test Exit");
+    }
+
+    public void onClearAllPressed() {
+        System.out.println("test clear All");
+    }
+
+    public void onCheckPressed() {
+        System.out.println("test check");
+    }
+
+    public void onRulesPressed() {
+        System.out.println("test Rules");
+    }
+
+    public void onButtonPressed(int index) {
+        System.out.println("test button " + index);
+    }
+
+    public void onClearButtonPressed() {
+        System.out.println("Test clear button");
+    }
+
+    public  void onHintPressed() {
+        System.out.println("Test hint");
+    }
+
+
+
+
+
 
 
 
